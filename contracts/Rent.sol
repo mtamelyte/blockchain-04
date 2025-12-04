33b97e862acfbe9ad8fe616ef8f063c8fcce4066
monikaaat <194424400+mtamelyte@users.noreply.github.com>
2025-12-04 20:40:41 +0200

    Create all key variables

--- contracts/Rent.sol
+++ contracts/Rent.sol
@@ -1,7 +1,27 @@
 pragma solidity ^0.8.0;
 
-contract Rent{
+contract RentalAgreement{
    address public landlord;
    address public tenant;
    address public propertyManager;
+
+   uint public rentCost;
+   uint public securityDeposit;
+   uint public managementFee;
+   
+   uint public lastPayment;
+   uint public depositHeld;
+   bool public isActive;
+
+   uint public start;
+   uint public end;
+
+   struct Payment{
+      uint id;
+      uint value;
+   }
+
+   Payment[] public paidRent;
+
+
 }
